968ecaa871c6c655cd811334916efea0dedef205
36 <tvhoang1@cmcglobal.vn>
2022-06-06 20:13:07 +0700

    new source

---
+++ demo/maintabs.js
@@ -0,0 +1,18 @@
+
+   $(function(){
+      $('#tabcontent').load("../MenuContentItem/tabcontent-1.html");
+      $('#tabcontent-2').load("../MenuContentItem/tabcontent-2.html"); 
+   });
+$(document).ready(function(){
+    $(".tab-content-item").hide();
+    $(".tab-content-item:first-child").fadeIn();
+    $(".nav-tabs li").click(function(){ 
+        $(".nav-tabs li").removeClass("active");
+        $(this).addClass("active");
+        $(".tab-content-item").hide();
+        // show tab content
+        let id_item_content = $(this).children("a").attr("href");
+        $(id_item_content).fadeIn();
+        return false;
+    })
+})
